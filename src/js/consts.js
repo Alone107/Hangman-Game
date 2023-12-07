aec342985d0c318903ded7fecc5cee60559eee5c
Alone107 <AloneOmen@yandex.ru>
2023-12-07 12:16:37 +0500

    Add hangman game files

---
+++ src/js/consts.js
@@ -0,0 +1,3 @@
+export const WORDS = ['code', 'frontend', 'programming', 'learn','london', 'home', 'hogwarts', 'word', 'water', 'sentence', 'picture', 'mother', 'answer', 'school', 'actual', 'different', 'air', 'animal', 'brother', 'money', 'order', 'piece', 'question', 'thriller', 'sound', 'mountain', 'love']
+
+export const KEYBOARD_LETTERS = Array.from('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
